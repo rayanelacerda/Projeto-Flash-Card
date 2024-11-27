c2f77075ec917969b810208970b331e7064ac419
rayanelacerda <loladanany@gmail.com>
2024-11-26 22:22:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula usada para resolver uma função quadrática?',
+    'A temida Bháskara; (-b±√(b²-4ac))/(2a)'
+)
+
+criaCartao(
+    'História',
+    'Quantos anos durou a guerra de 100 anos?',
+    'Apesar do nome, a guerra não durou exatamente cem anos. O conflito entre os dois países aconteceu de 1337 a 1453, ou seja, 116 anos'
+)
+
+criaCartao(
+    'Programação',
+    'Cite algumas linguagens de programção',
+    'JavaScript, Python, Java, PHP, CSS, C#, C++ e C.'
+)
+
+criaCartao(
+    'Física',
+    'Qual lei de Newton é possível calcular força?',
+    'Segunda Lei Newton (F = m.a)'
+)
